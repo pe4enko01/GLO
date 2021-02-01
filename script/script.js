b8a8968b4ad15353ba9cfd496b2f9c56a570aa63
pe4enko01 <pe4enko02@yandex.ru>
2021-02-01 11:11:44 +0300

    First lesson

--- script/script.js
+++ script/script.js
@@ -1,3 +1,3 @@
 let money, income, addExpenses, deposit, mission, period;
-alert("сообщение с любым текстом");
-console.log("сообщение с любым текстом");
+alert("Cообщение с любым текстом");
+console.log("Cообщение с любым текстом");
