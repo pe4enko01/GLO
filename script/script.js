998b091cd34cd36548c3f3d3360aae3b6ae459e5
pe4enko01 <pe4enko02@yandex.ru>
2021-02-02 10:34:53 +0300

    lesson two

--- script/script.js
+++ script/script.js
@@ -1,3 +1,18 @@
-let money, income, addExpenses, deposit, mission, period;
-alert("Cообщение с любым текстом");
-console.log("Cообщение с любым текстом");
+let money = 3,
+income = "Паперть",
+ddExpenses = "Лобстеры, аренда виллы, зарплата прислуге", 
+deposit = true,
+mission = 1,
+period = 8;
+//alert("Cообщение с любым текстом");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(ddExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} зимбабвийский долларор`);
+
+console.log(ddExpenses.toLocaleLowerCase().split(', '));
+
+let budgetDay = money/30;
+console.log(budgetDay);
