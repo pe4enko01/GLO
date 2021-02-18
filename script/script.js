8f3cda20d4f5812a95f1f5c032c754a29afef158
pe4enko01 <pe4enko02@yandex.ru>
2021-02-18 11:04:40 +0300

    7

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let isNumber = function (n) {
-    return !isNaN(parseFloat(n) && isFinite(n));
+    return !isNaN(Number(n));
 };
 let money;
 let start = function () {
@@ -14,11 +14,17 @@ start();
 
 let appData = {
     budget: money,
-    mission: 7,
+    deposit: true,
+    mission: 700000,
+    period:10,
+    ddExpenses:0,
     budgetDay: 0,
     budgetMonth: 0,
     expensesMonth: 0,
+    percentDeposit:0,
+    moneyDeposit:0,
     expenses: {},
+    income: {},
 
     getExpensesMonth: function () {
         for (let key in appData) {
@@ -41,10 +47,36 @@ let appData = {
         }
     },
     asking: function () {
+
+        let addExp = prompt("Перечислите возможные расходы за рассчитываемый период через запятую", 
+        "Лобстеры, аренда виллы, зарплата прислуге");
+        while (!isNaN(Number(addExp))){
+            addExp = prompt("Перечислите возможные расходы за рассчитываемый период через запятую", 
+            "Лобстеры, аренда виллы, зарплата прислуге");
+        };
+        appData.ddExpenses = `[${addExp.toLocaleLowerCase().split(', ')}]`;
+
+        if(confirm("Есть ли у вас дополнительный заработок?")){
+            
+            let itemIncome = prompt("Какой у вас дополнительный заработок?", "Таксую");
+            while (!isNaN(Number(itemIncome))){
+                itemIncome = prompt("Какой у вас дополнительный заработок?", "Таксую");
+            };
+            
+            let cashIncome = prompt("Сколько в месяц вы на этом зарабатываете?", 10000);
+            while (!isNumber(cashIncome)){
+                cashIncome = prompt("Сколько в месяц вы на этом зарабатываете?", 10000);
+            };
+            appData.income[itemIncome] = cashIncome; 
+        };
+        appData.deposit = confirm("Есть ли у вас депозит в банке?")
         for (let i = 0; i < 2; i++) {
-            let qustion = prompt(`Введите обязательную ${i === 0 ? "Первую" : "Вторую"} статью расходов?`);
+            let qustion = prompt(`Введите обязательную ${i === 0 ? "Первую" : "Вторую"} статью расходов?`, );
+            while (!isNaN(Number(qustion))){
+                qustion = prompt(`Введите обязательную ${i === 0 ? "Первую" : "Вторую"} статью расходов?`);
+            };     
             do {
-                appData.expenses[qustion] = prompt(`Во сколько обойдется ${i === 0 ? "Первая" : "Вторая"} статья?`);
+                appData.expenses[qustion] = parseFloat(prompt(`Во сколько обойдется ${i === 0 ? "Первая" : "Вторая"} статья?`));
             }
             while (!isNumber(appData.expenses[qustion]));
         }
@@ -59,6 +91,23 @@ let appData = {
         } else if (0 > appData.budgetDay) {
             return ("Что-то пошло не так");
         }
+    },
+    getInfoDeposit: function () {
+        if(appData.deposit){
+            do  {
+                appData.percentDeposit = prompt("Какой годовой процент?", "10");
+            }
+            while (!isNumber(appData.percentDeposit));
+
+            appData.moneyDeposit = prompt("Какая сумма заложена?", 10000);
+            while (!isNumber(appData.moneyDeposit)){
+                appData.moneyDeposit = prompt("Какая сумма заложена?", 10000);
+            };
+
+        }
+    },
+    calcSaveMoney: function(){
+        return appData.budgetMonth  * appData.period;
     }
 };
 
@@ -66,23 +115,26 @@ appData.asking();
 appData.getExpensesMonth();
 appData.getBudget();
 appData.getStatusIncome();
+appData.getInfoDeposit();
+appData.calcSaveMoney();
 
 console.log(`Расходы за месяц ${appData.expensesMonth}`);
 console.log(`${appData.getTargetMonth()}`);
 console.log(appData.getStatusIncome());
 
+console.log(appData.percentDeposit, appData.moneyDeposit,appData.calcSaveMoney());
 
 console.log(`Наша программа включает в себя данные:`);
 for (let key in appData) {
-    if(typeof(appData[key]) !== 'function'){
+    
         if (typeof(appData[key]) === 'object') {
             let popr = "";
             for (let i in appData[key]) {
-                popr +=`${i} : ${appData[key][i]}, `;
+                popr +=`${i}: ${appData[key][i]}, `;
             }
-            console.log(`${key} : {${popr.slice(0,length-2)}}`);
+            console.log(`${key}: {${popr.slice(0,length-2)}}`);
         } else {
-            console.log(`${key} : ${appData[key]}`)
+            console.log(`${key}: ${appData[key]}`)
         }
-    }
+    
 }
