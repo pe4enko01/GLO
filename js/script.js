1f09600b0f8de4a405608f65dc0403e6c732034c
pe4enko01 <pe4enko02@yandex.ru>
2021-02-25 14:27:09 +0300

    lesson11

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let books = document.querySelector(".books");
-let book = document.getElementsByClassName("book");
-
-//1
-books.prepend(book[1]);
-books.append(book[2]);
-book[2].before(book[3]);
-
-//2
