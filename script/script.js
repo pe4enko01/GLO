819e92d132d70159b0df73c8eed0f95bc0997727
pe4enko01 <pe4enko02@yandex.ru>
2021-03-09 14:23:55 +0300

    l15

--- script/script.js
+++ script/script.js
@@ -20,10 +20,16 @@ let expendedItem = document.querySelectorAll(".expenses-items");
 const additionalExpensesTitle = document.querySelector('.additional_expenses-item');
 const depositAmount = document.querySelector('.deposit-amount');
 const depositPercent = document.querySelector('.deposit-percent');
+const depositCheckmark = document.querySelector('#deposit-check');
 const dtargetAmount = document.querySelector('.target-amount');
 const  periodSelect = document.querySelector('.period-select');
 const  incomePeriodValue = document.querySelector('.income_period-value');
 let incomeItem = document.querySelectorAll(".income-items");
+const depositBank = document.querySelector('.deposit-bank');
+const depositAmountInput = document.querySelector('.deposit-amount');
+const depositPercentInput = document.querySelector('.deposit-percent');
+
+
 
 class AppData {
     constructor(){
@@ -95,7 +101,7 @@ class AppData {
             });
     };
     showResult(){
-        budgetMonthValue.value = this.budget;
+        budgetMonthValue.value = this.budgetMonth;
         budgetDayValue.value = Math.ceil(this.budget/30);
         expensesMonthValue.value =  this.expensesMonth;
         additionalExpenses.value =  this.ddExpenses.join(', ');
@@ -177,7 +183,8 @@ class AppData {
         }
     };
     getBudget() {
-        this.budgetMonth = +this.budget - +this.expensesMonth;
+        const monthDeposit = this.moneyDeposit *(this.percentDeposit/100);
+        this.budgetMonth = +this.budget - +this.expensesMonth + monthDeposit;
         this.budgetDay = Math.floor(this.budgetMonth / 30);
     };
     getTargetMonth() {     
@@ -191,12 +198,14 @@ class AppData {
         if (this.toggle === true){
             this.budget = +salaryAmount.value;
             this.changeButton();
+            this.getInfoDeposit();
             this.getexpenses();
             this.getIncome();
             this.getExpensesMonth();
             this.getMonthBubgetPlusIncome();
             this.getAddExpenses();
             this.getAddIncome();
+            this.getBudget();
             this.showResult();
             this.blockInputs();
         }else{
@@ -287,6 +296,37 @@ class AppData {
         this.toggle= !this.toggle;
         
     };
+    getInfoDeposit(){
+        if(this.deposit === true){
+            this.percentDeposit = depositPercent.value;
+            this.moneyDeposit = depositAmount.value;
+            console.log("🚀 ~ file: script.js ~ line 303 ~ AppData ~ getInfoDeposit ~ this.moneyDeposit", this.moneyDeposit)
+        }
+    }
+    changePercent(){
+        const selectIndex = this.value;
+        if(selectIndex ==="other"){
+            // ДЗ
+        }else{
+            depositPercent.value = selectIndex;
+        }
+        console.log("🚀 ~ file: script.js ~ line 305 ~ AppData ~ changePercent ~ selectIndex", selectIndex);
+    }
+    depositHendler(){
+        if(depositCheckmark.checked){
+           depositBank.style.display = 'inline-block';
+           depositAmountInput.style.display = 'inline-block';
+           this.deposit = true;
+           depositBank.addEventListener("change", this.changePercent);
+        }else{
+            depositBank.style.display = 'none';
+           depositAmountInput.style.display = 'none';
+           depositBank.value = "";
+           depositAmountInput.value = "";
+           this.deposit = true;
+           depositBank.removeEventListener("change", this.changePercent);
+        }
+    };
     eventsListeners(){
         const _this = this;
         this.checkStatrButton();
@@ -294,12 +334,13 @@ class AppData {
         secondPlus.addEventListener("click", this.addExpensesdBlock.bind(_this));
         firstPlus.addEventListener("click",  this.addIncomeBlock.bind(_this));
         periodSelect.addEventListener("input",()=>{ 
-        document.querySelector('.period-amount').innerHTML = periodSelect.value;
+            document.querySelector('.period-amount').innerHTML = periodSelect.value;
         });
+        depositCheckmark.addEventListener("change", this.depositHendler.bind(_this));
     };
 }
-    
-    
-    const appData = new AppData();
-    appData.eventsListeners();
-    
+
+
+const appData = new AppData();
+appData.eventsListeners();
+
