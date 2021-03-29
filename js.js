97286a99d31c4bb2e6fc041ba108dc7e8b357561
pe4enko01 <pe4enko02@yandex.ru>
2021-03-29 13:00:29 +0300

    l24

--- js.js
+++ js.js
@@ -352,3 +352,46 @@ function rep(form2Name){
     }
 };
 replase();
+
+//калькулятор
+const calc = (price = 100)=>{
+    let calcType = document.querySelector(".calc-type"),
+    calcBlock = document.querySelector(".calc-block"),
+    calcSquare = document.querySelector(".calc-square"),
+    calcDay = document.querySelector(".calc-day"),
+    calcCount = document.querySelector(".calc-count"),
+    totalValue = document.getElementById("total");
+    
+    
+    const countSum = ()=>{
+        let total = 0;
+        let typeValue = calcType.options[calcType.selectedIndex].value;
+        let square = +calcSquare.value;
+        let countValue = 1;
+        let dayValue = 1;
+        
+        if(calcCount.value > 1){
+            countValue += (calcCount.value - 1) /10;
+        }
+       // console.log("🚀 ~ file: js.js ~ line 359 ~ calc ~ calcType", countValue);
+        if(calcDay.value && calcDay.value < 5){
+            dayValue *= 2;
+        }else if( calcDay.value && calcDay.value < 10){
+            dayValue *= 1.5;
+        }
+        if(typeValue && square){
+            total =Math.floor(price * typeValue * square * countValue * dayValue);
+        }
+
+        totalValue.textContent = total;
+    }
+    calcBlock.addEventListener('change', (event)=>{
+        const target = event.target;
+        if(target === calcType || target === calcSquare || 
+            target === calcDay || target === calcCount){
+                countSum()
+;       }
+    })
+}
+calc()
+
