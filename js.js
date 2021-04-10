c0b9a6a001cde0e03e92848bccda944c0312fda2
pe4enko01 <pe4enko02@yandex.ru>
2021-04-10 15:28:29 +0300

    l29

--- js.js
+++ js.js
@@ -477,10 +477,13 @@ const sendForm = ()=>{
     const form3Emeil = document.getElementById("form3-email");
     const form3Phone = document.getElementById("form3-phone");
     const popup = document.querySelector(".popup");
+    const popupContent = document.querySelector(".popup-content");
+ 
 
 
 
     const stausMessage = document.createElement("div");
+    const stausMessageForm = document.createElement("div");
    // stausMessage.textContent = "Тут будет сообщение";
    // stausMessage.style.cssText = "font-size: 2rem";
     form.addEventListener("submit", (event)=>{
@@ -497,18 +500,20 @@ const sendForm = ()=>{
      
             const post = postData(body)
                 post
-                .then(stausMessage=>{
-                  stausMessage.textContent = successMasage;
-                  return new Promise((resolve, reject)=>{
+                .then((response)=>{
+                    if(response.status !== 200){
+                        throw new Error("status network not 200"); 
+                    }  
+                 stausMessage.textContent = successMasage;
+                  })
+                  .then(()=>{
                     setTimeout(()=>{
-                        resolve();
+                        stausMessage.textContent = "";
                     },3000);
                   })
-                })
-                .then(()=>{
-                    stausMessage.textContent = "";
-                })
-                .catch();
+                  .catch(()=>{
+                    stausMessage.textContent = errorMessage;
+                  })
             form1Name.value = "";
             form1Emeil.value = "";
             form1Phone.value = "";
@@ -516,33 +521,44 @@ const sendForm = ()=>{
     });
      form3.addEventListener("submit", (event)=>{
         event.preventDefault();
+
         if(form3Name.value.length >= 2 && form3Phone.value.length >=7  && form3Emeil.value.indexOf("@") !== -1){
+            
+            popupContent.appendChild(stausMessageForm);
+            stausMessageForm.style.color = "white";
             form.appendChild(stausMessage);
             const formData =new FormData(form3);
             stausMessage.textContent = loadMessage;
+            stausMessageForm.textContent = loadMessage;
             let body = {};
              for(let val of formData.entries()){
                  body[val[0]] = val[1];
                 }
                 const post = postData(body)
                 post
-                .then(stausMessage=>{
-                  stausMessage.textContent = successMasage;
-                  return new Promise((resolve, reject)=>{
+                .then((response)=>{
+                    if(response.status !== 200){
+                        throw new Error("status network not 200"); 
+                    }  
+                 stausMessage.textContent = successMasage;
+                 stausMessageForm.textContent = successMasage;
+                  })
+                  .then(()=>{
                     setTimeout(()=>{
-                        resolve();
+                        stausMessage.textContent = "";
+                        stausMessageForm.textContent = "";
+                        popup.style.display = "none";
                     },3000);
                   })
-                })
-                .then(()=>{
-                    stausMessage.textContent = "";
-                })
-                .catch();
+                  .catch(()=>{
+                    stausMessage.textContent = errorMessage;
+                    stausMessageForm.textContent = errorMessage;
+                  })
     
             form3Name.value = "";
             form3Emeil.value = "";
             form3Phone.value = "";
-            popup.style.display = "none";
+            //popup.style.display = "none";
         }
      });
 
@@ -558,18 +574,20 @@ const sendForm = ()=>{
                 }
                 const post = postData(body)
                 post
-                .then(stausMessage=>{
-                  stausMessage.textContent = successMasage;
-                  return new Promise((resolve, reject)=>{
+                .then((response)=>{
+                    if(response.status !== 200){
+                        throw new Error("status network not 200"); 
+                    }  
+                 stausMessage.textContent = successMasage;
+                  })
+                  .then(()=>{
                     setTimeout(()=>{
-                        resolve();
+                        stausMessage.textContent = "";
                     },3000);
                   })
-                })
-                .then(()=>{
-                    stausMessage.textContent = "";
-                })
-                .catch();
+                  .catch(()=>{
+                    stausMessage.textContent = errorMessage;
+                  })
 
             form2Name.value = "";
             form2Emeil.value = "";
@@ -579,25 +597,14 @@ const sendForm = ()=>{
      });
 
     const postData = (body)=>{
-        return new Promise((resolve, reject)=>{
-            const request = new XMLHttpRequest();
-            request.addEventListener('readystatechange', ()=>{
-                
-                if(request.readyState !== 4){
-                    return;
-                }
-                if(request.status === 200){
-                    resolve(stausMessage);
-    
-                }else{
-                    reject("Error");
-                }
-            })
-            request.open('POST', "./server.php");
-            request.setRequestHeader('Content-Type', 'multipart/json');
-            
-            request.send(JSON.stringify(body));
-        })
+        return fetch('./server.php',{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'multipart/json'
+            },
+            body: JSON.stringify(body)
+
+        });
     }
    
 }
