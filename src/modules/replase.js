34d9991a54a3fe020412b800d5e4d7da0b3bbb79
pe4enko01 <pe4enko02@yandex.ru>
2021-04-13 13:48:33 +0300

    l30

--- src/modules/replase.js
+++ src/modules/replase.js
@@ -145,4 +145,23 @@ const replase = ()=>{
         form1Phone.value = form1Phone.value.replace(/\s+/g, '')
     })
 }
+
+function rep(form2Name){
+    for(let i = 0; i < form2Name.value.length; i++){
+        if(form2Name.value[0].indexOf("-") === 0){
+            form2Name.value = form2Name.value.slice(1);
+        }
+        if(form2Name.value[0].indexOf(" ") === 0){
+            form2Name.value = form2Name.value.slice(1);
+        }
+    }
+    for(let i = form2Name.value.length; i > 0 ; i--){
+        if(form2Name.value[form2Name.value.length-1].indexOf("-") === 0){
+            form2Name.value = form2Name.value.slice(0, form2Name.value.length -1);
+        }
+        if(form2Name.value[form2Name.value.length-1].indexOf(" ") === 0){
+            form2Name.value = form2Name.value.slice(0, form2Name.value.length -1 );
+        }
+    }
+};
 export default replase;
